8b92efd3ff10af7df6f07a7d0c2bd4871faf94b4
LoreLor <lorenadearmas01@gmail.com>
2024-07-11 21:38:42 -0300

    clase Serie - enum Categoria -constructor - casteos

--- src/main/java/com/aluracursos/screenmatch2/models/Categoria.java
+++ src/main/java/com/aluracursos/screenmatch2/models/Categoria.java
@@ -13,4 +13,14 @@ public enum Categoria {
     Categoria(String categoriaOmdb) {
         this.categoriaOmdb = categoriaOmdb;
     }
+
+    //parseo de enum a string
+    public static Categoria fromString(String text){
+        for (Categoria categoria : Categoria.values()) {
+            if(categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
+    }
 }
